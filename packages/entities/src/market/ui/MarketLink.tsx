40eb2ed5de880b5048f0ecba442f452ec61c774e
Alexey Lizurchik <al.lizurchik@gmail.com>
2025-12-18 14:24:26 +0100

    cli: add port numbers to "start" script

--- packages/entities/src/market/ui/MarketLink.tsx
+++ packages/entities/src/market/ui/MarketLink.tsx
@@ -4,7 +4,7 @@ import { BrandedLink, BrandedLinkProps } from '@repo/ui/branded-link';
 import { type FC } from 'react';
 import { useMarket } from '../provider';
 
-export interface Props extends BrandedLinkProps {}
+export interface Props extends Omit<BrandedLinkProps, 'prefix'> {}
 
 export const MarketLink: FC<Props> = (props) => {
   const { code } = useMarket();
